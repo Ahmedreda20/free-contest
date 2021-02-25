6aa818e32ab7424663edb35bfd5cd73b87586296
Ahmedreda20 <62707388+Ahmedreda20@users.noreply.github.com>
2021-02-25 13:17:15 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const accounts_box = document.querySelectorAll('.accounts_box .box');
+const searchform = document.querySelector('.search_form');
 
 accounts_box.forEach(account => {
    account.onclick = () => {
@@ -12,3 +13,25 @@ accounts_box.forEach(account => {
          account.setAttribute('tabindex', 0);
    }
 })
+searchform.onsubmit = e => {
+   e.preventDefault();
+}
+searchform['search'].onkeyup = ({ target , code }) => {
+
+   accounts_box.forEach(acc => {
+      var accname = acc.querySelector('.content').textContent;
+      var currentname = accname.toLowerCase();
+      var val = target.value.toLowerCase();
+
+      if (currentname.indexOf(val) >= 0) {
+         acc.classList.remove('hidden_box');
+
+         var re = accname.replace(new RegExp(val + '(?!([^<]+)?<)', 'gi'), `<span class="highlight">$&</span>`);
+          acc.querySelector('.content').innerHTML = re;
+      } else {
+          acc.classList.add('hidden_box');
+      }
+
+   })
+
+}
